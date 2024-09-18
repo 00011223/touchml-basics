7ffd78cd39fbdade9c5f0ccd2ca78c18afff2c4e
00011223 <wang999534liu1933@163.com>
2024-09-19 06:20:42 +0800

    update

---
+++ 20220703h5-miniprogram/miniprogram/pages/customAttr/customAttr.js
@@ -0,0 +1,95 @@
+// pages/customAttr/customAttr.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [{
+        id: 3,
+        name: '张三',
+        sex: 'male',
+        age: 17
+      },
+      {
+        id: 1,
+        name: '李四',
+        sex: 'female',
+        age: 24
+      },
+      {
+        id: 2,
+        name: '老王',
+        sex: 'other',
+        age: 30
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  },
+
+  // 单个删除
+  remove(ev) {
+    console.log(ev);
+    // 获取被删除的数据id
+    let id = ev.target.dataset.id
+    // 查找id所对应的数据索引
+    this.data.list.findIndex()
+    // 然后删除对应索引的数据
+
+  }
+})
