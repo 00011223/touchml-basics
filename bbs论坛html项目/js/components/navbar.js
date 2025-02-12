6e2189c936ea1ba7e7a3e2686dc3aa6527aac32f
00011223 <wang999534liu1933@163.com>
2025-02-12 20:49:54 +0800

    update

---
+++ bbs论坛html项目/js/components/navbar.js
@@ -0,0 +1,47 @@
+function Navbar(props) {
+    // 是否登录
+    const [isSignIn, setIsSignIn] = React.useState(false);
+    const [nickname, setNickname] = React.useState('张三');
+    const [headIcon, setHeadIcon] = React.useState('../../img/avatar-max-img.png');
+    const [kw, setKw] = React.useState('');
+
+    return (
+        <div style={{height: '48px', backgroundColor: '#f8f8f8'}}>
+            <div className="container h-100 d-flex align-items-center justify-content-between">
+                {/* 左侧内容 */}
+                <div className="d-flex">
+                    <img src="./img/logo.png" style={{height: '32px'}} className="me-3"/>
+                    <input value={kw} onInput={ev => {
+                        setKw(ev.target.value)
+                    }} type="text" className="form-control form-control-sm" placeholder="搜索"/>
+                </div>
+                {/* 右侧内容 */}
+                {isSignIn ? (
+                        <div className="d-flex align-items-center">
+                            <span>欢迎，{nickname}!</span>
+                            {/* 下拉菜单 */}
+                            <div className="dropdown">
+                                <button className="btn dropdown-toggle" type="button"
+                                        data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img style={{width: '32px'}} src={headIcon}/>
+                                </button>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">个人中心</a></li>
+                                    <li><a className="dropdown-item" href="#">设置</a></li>
+                                    <li><a className="dropdown-item" href="#">退出</a></li>
+                                </ul>
+                            </div>
+                            <button className="btn btn-outline-secondary">发起</button>
+                        </div>
+                    ) :
+                    (
+                        <div>
+                            <button onClick={()=>{location.href='./signIn.html'}} className="btn btn-success me-2">登录</button>
+                            <button onClick={()=>{location.href='./signUp.html'}} className="btn btn-primary">注册</button>
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
