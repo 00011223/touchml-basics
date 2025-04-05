08d2c6a72b4aa79f87829e9877ae14b7b723fd31
00011223 <wang999534liu1933@163.com>
2025-04-05 21:22:15 +0800

    update

---
+++ nodejs/day05/AJAX/02_axios简介.js
@@ -0,0 +1,27 @@
+/* 
+    在客户端可以发起ajax请求，除了使用XMLHttpRequst和fetch方法外，还可以使用
+    当前比较成熟且流行的第三放库，其中最为突出的就是Axios网络请求库！
+
+
+    axios是什么？
+        Axios是一个基于promise的网络请求库，可以作用余nodejs和浏览器中
+
+
+    特点：简单、快、支持promise
+
+    axios安装: 
+    - 1-通过命令 npm install axios 进行安装
+    - 2-通过在线网络资源，CDN
+    - 3-将axios源文件下载到本地，引入使用
+
+    
+
+
+
+
+
+
+
+
+
+*/
---
+++ nodejs/day05/AJAX/03_axios实现ajax请求/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.use('/public', express.static(`${__dirname}/public`))
+app.get('/', (req, res) => res.send('Hello World!'))
+
+// 原生ajax请求 '/get_xhr'接口
+app.get('/get_axios', (req, res) => {
+  res.send({
+    msg: '我收到你的数据啦',
+    data: req.query,
+  })
+})
+
+// 原生ajax请求 '/post_xhr'接口
+app.post('/post_axios', (req, res) => {
+  res.send({
+    msg: '我收到你的数据啦',
+    data: req.body,
+  })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
