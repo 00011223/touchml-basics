b092e40175e71524ac4660b96085fdd934e9387a
00011223 <wang999534liu1933@163.com>
2025-07-01 15:06:36 +0800

    update

--- 企业级开发/vue3/组件/src/components/SlotChild.vue
+++ 企业级开发/vue3/组件/src/components/SlotChild.vue
@@ -8,18 +8,40 @@
         </div>
         <div>
             <!-- 匿名插槽
+                匿名插槽在组件中只能有一个
                 slot 标签体的内容将称为插槽的默认值
              -->
-            <slot>content</slot>
+<!--            <slot>content</slot>-->
+            content
         </div>
         <div>
             footer
+
+
+            <!-- 作用域插槽
+                给普通的插槽进行属性绑定，则该插槽将变成作用域插槽
+                无论是匿名插槽还是具名插槽都可以成为作用域插槽
+             -->
+
+            <!-- n: 是一个作用域插槽的变量名 是自定义的
+                name: 是一个希望外部能够访问到的参数
+             -->
+            <slot :n="name" :s="sex"></slot>
+
+            <slot name="footer" :a="age"></slot>
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            name: '张三',
+            sex: 'female',
+            age: 17
+        }
+    },
     // render(){
     //     return h('div', null, this.slots.default)
     // },
--- 企业级开发/vue3/组件/src/components/插槽.vue
+++ 企业级开发/vue3/组件/src/components/插槽.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <SlotChild>
+
             <!-- 在组件标签体里写的内容 默认被插入匿名插槽 -->
             <!-- 插槽内若不提供内容 则将使用插槽的默认值 -->
 
@@ -10,9 +11,9 @@
             <!-- 若已经使用了具名插槽 又想使用匿名插槽的话， 请使用template标签
                 插槽名使用 default
              -->
-            <template #default>
+<!--            <template #default>
                 this is content
-            </template>
+            </template>-->
 
 
             <!-- 使用template标签来使用具名插槽
@@ -27,6 +28,24 @@
                     &nbsp;
                     具名插槽
                 </span>
+            </template>
+
+
+
+
+            <!-- 使用匿名的作用域插槽 -->
+            <!-- props 是变量名可以自定义
+                props 代表的是 slot 标签上的属性
+             -->
+            <template #default="props">
+                <ul>
+                    <li>姓名: {{props.n}}</li>
+                    <li>性别: {{props.s}}</li>
+                </ul>
+            </template>
+
+            <template #footer="props">
+                年龄: {{props.a}}
             </template>
         </SlotChild>
     </div>
@@ -44,6 +63,10 @@
 
 // 什么是插槽？
 // 插槽用来描述组件的标签体内能够填充哪些内容，并且插槽规定了组件标签体填入到组件的哪个地方
+
+// 什么是作用域插槽
+// 当自定义组件的标签体内想要访问该自定义组件的属性时，可以使用作用域插槽
+
 
 import SlotChild from "@/components/SlotChild.vue";
 
