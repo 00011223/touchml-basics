0c61d1cc4b480fb2d8799f29523dbb852f455132
00011223 <wang999534liu1933@163.com>
2025-07-22 04:15:56 +0800

    update

--- 企业级开发/vuex@4/vuex-demo/src/store/基础用法.js
+++ 企业级开发/vuex@4/vuex-demo/src/store/基础用法.js
@@ -5,6 +5,11 @@
 //      3. 至少配置 state mutations actions
 //      4. 导出 store
 //      5. 在 main.js 中 安装 store 插件
+// 页面中读取状态
+// 修改状态
+//      1. 通过commit调用mutation修改状态
+//      2. 通过dispatch调用action修改状态
+// 异步 action 的使用，返回值的获取
 
 // 1. 引入 createStore
 import {createStore} from 'vuex'
@@ -20,9 +25,43 @@ const store = createStore({
         }
     },
     // 2. mutations 用来修改状态的方法集合
-    mutations: {},
+    mutations: {
+        // nickname属性的mutation
+        // 这一个mutation用于修改 nickname 状态
+        mNickname(state, payload) {
+            // state: store对象的当前状态
+            // payload: 承载的数据
+            state.nickname = payload
+        }
+    },
     // 3. actions 用来触发状态更新的操作集合
-    actions: {}
+    actions: {
+        // 此action用于操作nickname
+        // aNickname(store, payload) {
+        //     // store: 当前仓库对象
+        //     // payload: 承载的数据
+        //     // 调用 commit 通过 mutation 修改昵称
+        //     store.commit('mNickname', payload)
+        // }
+
+
+        // 异步的action
+        async aNickname(store, payload) {
+            // 假装做网络请求
+            // result 就是网络请求的结果
+            let result = await new Promise(resolve => {
+                setTimeout(() => {
+                    // 假装接收到网络请求的返回值
+                    // 并将请求结果保存到状态中
+                    store.commit('mNickname', payload)
+                    resolve(payload)
+                }, 2000)
+            })
+
+            // action 返回请求得到的结果
+            return result
+        }
+    }
 })
 
 // 3. 导出 store
--- 企业级开发/vuex@4/vuex-demo/src/views/基础用法.vue
+++ 企业级开发/vuex@4/vuex-demo/src/views/基础用法.vue
@@ -2,14 +2,37 @@
     <div>
         <h1>基础用法</h1>
         <div>
+            <!-- 页面中读取状态 -->
             <!-- $store 是组件对象内置的 store 仓库对象 -->
             nickname: {{ $store.state.nickname }}
+        </div>
+        <div>
+            <input type="text" :value="$store.state.nickname" @input="onNicknameInput">
         </div>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+    methods: {
+        onNicknameInput(ev) {
+            // 1. 通过commit调用mutation修改状态
+            // 注意: 按规范来说，页面不应该直接调用 mutation
+            // this.$store.commit('mNickname', ev.target.value)
+
+            // 2. 通过dispatch调用action修改状态
+            // this.$store.dispatch('aNickname', ev.target.value)
+
+
+            // 调用异步action并获得返回值
+            // dispatch 方法将返回一个promise对象
+            this.$store.dispatch('aNickname', ev.target.value).then(data => {
+                // 此处的data就是 aNickname action 的返回值
+                console.log(data)
+            })
+        }
+    }
+}
 </script>
 
 <style scoped>
