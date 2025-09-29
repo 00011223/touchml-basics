af7925bef7b7c6788400e7adb77a5a9803e16939
00011223 <wang999534liu1933@163.com>
2025-09-30 02:10:17 +0800

    update

--- 企业级开发/vuex@4/vuex-demo/src/store/user.js
+++ 企业级开发/vuex@4/vuex-demo/src/store/user.js
@@ -7,17 +7,39 @@ export default {
     namespaced: true,
     state() {
         return {
-            age: 17
+            age: 17,
+            count: 0,
+            money: 500
         }
     },
     mutations: {
         mAge(state, payload) {
             state.age = payload
-        }
+        },
+        mCount(state, payload) {
+            state.count = payload
+        },
+        mMoney(state, payload) {
+            state.money = payload
+        },
     },
     actions: {
         aAge({commit}, payload) {
             commit('mAge', payload)
-        }
+        },
+        aCount: {
+            // root 为 true 代表将此 action 注册到根节点
+            root: true,
+            // handler 为 action 具体的处理函数
+            handler(context, payload) {
+                console.log(context)
+                // 模块内可以通过 context.rootGetters 和 context.rootState 来访问根节点的 getters 和 state\
+                console.log(context.rootState)
+                context.commit('mCount', payload)
+            }
+        },
+        aMoney({commit}, payload) {
+            commit('mMoney', payload)
+        },
     }
 }
--- 企业级开发/vuex@4/vuex-demo/src/views/Module.vue
+++ 企业级开发/vuex@4/vuex-demo/src/views/Module.vue
@@ -6,14 +6,25 @@
         <!-- state 中使用模块的名称访问模块里的状态 -->
         <div>age: {{ $store.state.user.age }}</div>
         <input type="number" v-model.number="age">
+        <div>count: {{ $store.state.user.count }}</div>
+        <input type="number" v-model.number="count">
+        <div>money: {{ money }}</div>
+        <input type="text" :value="money" @input="ev=>{ aMoney(ev.target.value) }">
     </div>
 </template>
 
 <script>
 import {defineComponent} from 'vue'
+import {createNamespacedHelpers, mapState, mapActions} from 'vuex'
+
+// 通过 createNamespacedHelpers 创建带有命名空间的 mapState 和 mapActions
+// const {mapState, mapActions} = createNamespacedHelpers('user')
 
 export default defineComponent({
     computed: {
+        // ...mapState(['money']),
+        // 第一个参数: 命名空间
+        ...mapState('user', ['money']),
         age: {
             get() {
                 return this.$store.state.user.age
@@ -24,7 +35,20 @@ export default defineComponent({
                 // 访问 user 模块中带有命名空间的 action
                 this.$store.dispatch('user/aAge', value)
             }
+        },
+        count: {
+            get() {
+                return this.$store.state.user.count
+            },
+            set(value) {
+                this.$store.dispatch('aCount', value)
+            }
         }
+    },
+    methods: {
+        // ...mapActions(['aMoney'])
+        // 第一个参数: 命名空间
+        ...mapActions('user', ['aMoney'])
     }
 })
 </script>
